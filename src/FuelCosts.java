3b282209621c04a2a2b992cab3707ee62f4e2d4c
Spencer <salane2232@gmail.com>
2025-09-28 13:15:57 -0400

    Do and else commit

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class FuelCosts {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+       double gallons = 0.0;
+       double mpg = 0.0; //miles per gallon
+       double ppg = 0.0; //price per gallon
+       boolean validInput;
+
+       validInput = false;
+        do {
+            System.out.print("Enter the gallon amount of gas in the tank: ");
+            if (scanner.hasNextDouble()) {
+                gallons = scanner.nextDouble();
+                validInput = true;
+
+            } else {
+                System.out.println("Invalid input, please enter a valid number");
+                scanner.next(); //take input
+            }
+        } while (!validInput);
+
+
+    }
 }
